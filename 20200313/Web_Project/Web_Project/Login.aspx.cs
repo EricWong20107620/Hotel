a482bef5bbb6ce5698003b8c0a6f837ae1f3302f
Eric Wong <zerosiuyiu@gmail.com>
2020-03-13 21:21:24 +0800

    Complete_Register_Login

---
+++ 20200313/Web_Project/Web_Project/Login.aspx.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Project
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        public SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Hotel"].ConnectionString);
+        public SqlConnection conn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["Hotel"].ConnectionString);
+        string questino_no;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear();
+
+            if (IsPostBack)
+            {
+                questino_no = lblQuestion.Text.Substring(1, 1);
+            }
+
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            builder.Append(random.Next(1, 4));
+            string randomNo = builder.ToString();
+
+            if (randomNo == "1")
+            {
+                lblQuestion.Text = "Q1. What’s your hobby ?";
+            }
+            else if (randomNo == "2")
+            {
+                lblQuestion.Text = "Q2. What’s your favorite country ?";
+            }
+            else
+            {
+                lblQuestion.Text = "Q3. Where are you born?";
+            }
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text) == true)
+            {
+                lblMessage.Text = "Please enter your email";
+                lblMessage.ForeColor = Color.Red;
+                txtEmail.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPassword.Text) == true)
+            {
+                lblMessage.Text = "Please enter your password";
+                lblMessage.ForeColor = Color.Red;
+                txtPassword.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtQuestion.Text) == true)
+            {
+                lblMessage.Text = "Please answer the security question";
+                lblMessage.ForeColor = Color.Red;
+                txtQuestion.Focus();
+                return;
+            }
+
+            DataTable dt = new DataTable();
+            dt = sp_login(txtEmail.Text, questino_no);
+
+            if (dt.Rows.Count == 0)
+            {
+                lblMessage.Text = "This email has not register";
+                lblMessage.ForeColor = Color.Red;
+                txtEmail.Text = "";
+                txtPassword.Text = "";
+                txtQuestion.Text = "";
+                txtEmail.Focus();
+                return;
+            }
+            else
+            {
+                if (dt.Rows[0].Field<int>("user_status") == 1)
+                {
+                    if (dt.Rows[0].Field<string>("login_password") != txtPassword.Text)
+                    {
+                        if (dt.Rows[0].Field<int>("count_wrong") < 2)
+                        {
+                            lblMessage.Text = "Email or password is not correct<br/>Remain " + (3 - (dt.Rows[0].Field<int>("count_wrong") + 1)) + " chance";
+                            lblMessage.ForeColor = Color.Red;
+                            sp_lock_account(txtEmail.Text, 1);
+                        }
+                        else if (dt.Rows[0].Field<int>("count_wrong") == 2)
+                        {
+                            lblMessage.Text = "Your account has been locked<br/>Please contact admin";
+                            lblMessage.ForeColor = Color.Red;
+                            sp_lock_account(txtEmail.Text, 2);
+                        }
+                        else
+                        {
+                            lblMessage.Text = "Your account has been locked<br/>Please contact admin";
+                            lblMessage.ForeColor = Color.Red;
+                        }
+                        txtEmail.Text = "";
+                        txtPassword.Text = "";
+                        txtQuestion.Text = "";
+                        txtEmail.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        if (dt.Rows[0].Field<string>("question_answer") != txtQuestion.Text)
+                        {
+                            if (dt.Rows[0].Field<int>("count_wrong") < 2)
+                            {
+                                lblMessage.Text = "Security question is not correct<br/>Remain " + (3 - (dt.Rows[0].Field<int>("count_wrong") + 1)) + " chance";
+                                lblMessage.ForeColor = Color.Red;
+                                sp_lock_account(txtEmail.Text, 1);
+                            }
+                            else if (dt.Rows[0].Field<int>("count_wrong") == 2)
+                            {
+                                lblMessage.Text = "Your account has been locked<br/>Please contact admin";
+                                lblMessage.ForeColor = Color.Red;
+                                sp_lock_account(txtEmail.Text, 2);
+                            }
+                            else
+                            {
+                                lblMessage.Text = "Your account has been locked<br/>Please contact admin";
+                                lblMessage.ForeColor = Color.Red;
+                            }
+                            txtQuestion.Text = "";
+                            txtQuestion.Focus();
+                            return;
+                        }
+                        else
+                        {
+                            lblMessage.Text = "Login Successful";
+                            lblMessage.ForeColor = Color.Green;
+                            sp_lock_account(txtEmail.Text, 0);
+                        }
+                    }
+                }
+                else if (dt.Rows[0].Field<int>("user_status") == 2)
+                {
+                    lblMessage.Text = "Your account has been locked<br/>Please contact admin";
+                    lblMessage.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblMessage.Text = "This email has not register";
+                    lblMessage.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        public DataTable sp_login(string email, string questionNo)
+        {
+            using (conn)
+            {
+                SqlCommand myCommand = new SqlCommand("sp_login", conn);
+
+                myCommand.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar));
+                myCommand.Parameters.Add(new SqlParameter("@questionNo", SqlDbType.VarChar));
+
+                myCommand.Parameters["@email"].Value = txtEmail.Text;
+                myCommand.Parameters["@questionNo"].Value = questino_no;
+
+                myCommand.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(myCommand);
+
+                DataTable dt = new DataTable();
+                sqlAdapter.Fill(dt);
+                conn.Close();
+                myCommand.Dispose();
+
+                return dt;
+            }
+        }
+
+        public int sp_lock_account(string email, int flag)
+        {
+            using (conn2)
+            {
+                try
+                {
+                    conn2.Open();
+                    SqlParameter[] parms = { new SqlParameter("@email", email),
+                                                new SqlParameter("@flag", flag) };
+                    SqlCommand Command = new SqlCommand("sp_lock_account", conn2);
+                    Command.CommandType = CommandType.StoredProcedure;
+                    Command.Parameters.AddRange(parms);
+                    return Command.ExecuteNonQuery();
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ 20200313/Web_Project/Web_Project/Register.aspx.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Web_Project
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        public SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Hotel"].ConnectionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session.Clear();
+            if (IsPostBack)
+            {
+                if (!(string.IsNullOrEmpty(txtPassword.Text)))
+                {
+                    txtPassword.Attributes["value"] = txtPassword.Text;
+                }
+
+                if (!(string.IsNullOrEmpty(txtConfirmPassword.Text)))
+                {
+                    txtConfirmPassword.Attributes["value"] = txtConfirmPassword.Text;
+                }
+            }
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text) == true)
+            {
+                lblMessage.Text = "Please enter your email";
+                lblMessage.ForeColor = Color.Red;
+                txtEmail.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtFirstName.Text) == true)
+            {
+                lblMessage.Text = "Please enter your first name";
+                lblMessage.ForeColor = Color.Red;
+                txtFirstName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtLastName.Text) == true)
+            {
+                lblMessage.Text = "Please enter your last name";
+                lblMessage.ForeColor = Color.Red;
+                txtLastName.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPhone.Text) == true)
+            {
+                lblMessage.Text = "Please enter your phone number";
+                lblMessage.ForeColor = Color.Red;
+                txtPhone.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPassport.Text) == true)
+            {
+                lblMessage.Text = "Please enter your passport number";
+                lblMessage.ForeColor = Color.Red;
+                txtPassport.Focus();
+                return;
+            }
+
+            if (txtPassword.Enabled == true)
+            {
+                if (string.IsNullOrEmpty(txtPassword.Text) == true)
+                {
+                    lblMessage.Text = "Please enter your password";
+                    lblMessage.ForeColor = Color.Red;
+                    txtPassword.Focus();
+                    return;
+                }
+                else
+                {
+                    if (txtConfirmPassword.Text != txtPassword.Text)
+                    {
+                        lblMessage.Text = "Confirm password is not correct";
+                        lblMessage.ForeColor = Color.Red;
+                        txtConfirmPassword.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        if (!(txtPassword.Text.Length >= 8 && txtPassword.Text.Length <= 15 && txtPassword.Text.Any(char.IsDigit) && txtPassword.Text.Any(char.IsLetter) && (txtPassword.Text.Any(char.IsSymbol) || txtPassword.Text.Any(char.IsPunctuation))))
+                        {
+                            lblMessage.Text = "Password length : 8 to 15 characters<br/>At leaset one lower-case letter<br/>At leaset one upper-case letter<br/>At leaset one special character";
+                            lblMessage.ForeColor = Color.Red;
+                            txtPassword.Focus();
+                            return;
+                        }
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(txtQuestionOne.Text) == true)
+            {
+                lblMessage.Text = "Please answer question one";
+                lblMessage.ForeColor = Color.Red;
+                txtQuestionOne.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtQuestionTwo.Text) == true)
+            {
+                lblMessage.Text = "Please answer question two";
+                lblMessage.ForeColor = Color.Red;
+                txtQuestionTwo.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtQuestionThree.Text) == true)
+            {
+                lblMessage.Text = "Please answer question three";
+                lblMessage.ForeColor = Color.Red;
+                txtQuestionThree.Focus();
+                return;
+            }
+
+            if (btnRegister.Text == "Send Verify Email")
+            {
+                string gender;
+
+                if (ddlGender.SelectedValue.ToString() == "M")
+                {
+                    gender = "Mr.";
+                }
+                else
+                {
+                    gender = "Ms.";
+                }
+
+                StringBuilder builder = new StringBuilder();
+                Random random = new Random();
+                builder.Append(random.Next(100000, 999999));
+                string verifyCode = builder.ToString();
+
+                int result = sp_register(txtEmail.Text, txtPassword.Text, txtFirstName.Text, txtLastName.Text, txtPassport.Text, ddlGender.SelectedValue.ToString(), txtPhone.Text, txtQuestionOne.Text, txtQuestionTwo.Text, txtQuestionThree.Text, verifyCode);
+
+                if (result == 1)
+                {
+                    lblMessage.Text = "Verify code has been send to your email";
+                    lblMessage.ForeColor = Color.Blue;
+                    txtVerfiyCode.Focus();
+                    btnRegister.Text = "Register";
+                    txtEmail.Enabled = false;
+                    txtFirstName.Enabled = false;
+                    txtLastName.Enabled = false;
+                    ddlGender.Enabled = false;
+                    txtPhone.Enabled = false;
+                    txtPassport.Enabled = false;
+                    txtPassword.Enabled = false;
+                    txtConfirmPassword.Enabled = false;
+                    txtQuestionOne.Enabled = false;
+                    txtQuestionTwo.Enabled = false;
+                    txtQuestionThree.Enabled = false;
+                }
+                else if (result == -1)
+                {
+                    lblMessage.Text = "This email has been register";
+                    lblMessage.ForeColor = Color.Red;
+                    txtEmail.Focus();
+                    return;
+                }
+                else
+                {
+                    lblMessage.Text = "FAIL REGISTER. PLEASE CONTACT ADMIN";
+                    lblMessage.ForeColor = Color.Red;
+                    return;
+                }
+
+                try
+                {
+                    MailMessage message = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    message.From = new MailAddress("jate.hotel@gmail.com");
+                    message.To.Add(new MailAddress(txtEmail.Text));
+                    message.Subject = "JATE Hotel Register Account Verify Code - " + txtFirstName.Text + " " + txtLastName.Text;
+                    message.IsBodyHtml = true;
+                    message.Body = "Dear " + gender + " " + txtLastName.Text + ",<br/><br/>Your verify code is " + verifyCode + ".<br/><br/>Regards,<br/><br/>JATE Hotel";
+                    smtp.Port = 587;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("jate.hotel@gmail.com", "jateh0te!");
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "Fail to send the email.";
+                    lblMessage.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(txtVerfiyCode.Text) == true)
+                {
+                    lblMessage.Text = "Please enter your verify code";
+                    lblMessage.ForeColor = Color.Red;
+                    txtVerfiyCode.Focus();
+                    return;
+                }
+
+                int result = sp_check_verify_code(txtEmail.Text, txtVerfiyCode.Text);
+                if (result == 1)
+                {
+                    lblMessage.Text = "Register successful";
+                    lblMessage.ForeColor = Color.Green;
+                    btnRegister.Text = "Send Verify Email";
+                    txtEmail.Enabled = true;
+                    txtFirstName.Enabled = true;
+                    txtLastName.Enabled = true;
+                    ddlGender.Enabled = true;
+                    txtPhone.Enabled = true;
+                    txtPassport.Enabled = true;
+                    txtPassword.Enabled = true;
+                    txtConfirmPassword.Enabled = true;
+                    txtQuestionOne.Enabled = true;
+                    txtQuestionTwo.Enabled = true;
+                    txtQuestionThree.Enabled = true;
+                }
+                else if (result == -1)
+                {
+                    lblMessage.Text = "Verify code is wrong";
+                    lblMessage.ForeColor = Color.Red;
+                    txtVerfiyCode.Focus();
+                }
+                else
+                {
+                    lblMessage.Text = "FAIL REGISTER. PLEASE CONTACT ADMIN";
+                    lblMessage.ForeColor = Color.Red;
+                }
+            }
+        }
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        public int sp_register(string email, string password, string firstName, string lastName, string passport, string gender, string phoneNumber, string questionOne, string questionTwo, string questionThree, string verifyCode)
+        {
+            using (conn)
+            {
+                try
+                {
+                    conn.Open();
+                    SqlParameter[] parms = { new SqlParameter("@email", email),
+                                                new SqlParameter("@password", password),
+                                                new SqlParameter("@firstName", firstName),
+                                                new SqlParameter("@lastName", lastName),
+                                                new SqlParameter("@passport", passport),
+                                                new SqlParameter("@gender", gender),
+                                                new SqlParameter("@phoneNumber", phoneNumber),
+                                                new SqlParameter("@questionOne", questionOne),
+                                                new SqlParameter("@questionTwo", questionTwo),
+                                                new SqlParameter("@questionThree", questionThree),
+                                                new SqlParameter("@verifyCode", verifyCode) };
+                    SqlCommand Command = new SqlCommand("sp_register", conn);
+                    Command.CommandType = CommandType.StoredProcedure;
+                    Command.Parameters.AddRange(parms);
+                    return Command.ExecuteNonQuery();
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int sp_check_verify_code(string email, string verifyCode)
+        {
+            using (conn)
+            {
+                try
+                {
+                    conn.Open();
+                    SqlParameter[] parms = { new SqlParameter("@email", email),
+                                                new SqlParameter("@verifyCode", verifyCode) };
+                    SqlCommand Command = new SqlCommand("sp_check_verify_code", conn);
+                    Command.CommandType = CommandType.StoredProcedure;
+                    Command.Parameters.AddRange(parms);
+                    return Command.ExecuteNonQuery();
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
